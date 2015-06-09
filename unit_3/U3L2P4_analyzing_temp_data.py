0118c124fbcb91dc78c3ca729a21245deb021e9d
Janet Riley <jriley@janetriley.net>
2015-06-08 22:24:51 -0400

    Added comments, etc.

--- unit_3/U3L2P4_analyzing_temp_data.py
+++ unit_3/U3L2P4_analyzing_temp_data.py
@@ -10,15 +10,34 @@ content = wu.load_30_days()
 daily = wu.extract_daily_from_json(content)
 hourly = wu.extract_hourly_from_json(content)
 
+values = {
+    'high_max': daily['temperatureMax'].max(),
+    'low_max': daily['temperatureMax'].min(),
+    'mean_max': daily['temperatureMax'].mean(),
+    'median_max': daily['temperatureMax'].median(),
+    'greatest_delta_max': daily['changeMax'].max(),
+    'smallest_delta_max': daily['changeMax'].min(),
 
-s = daily.sort(columns='apparentTemperatureMinTime',ascending=True)
-
-print s['temperatureMax'].min()
-print s['temperatureMax'].max()
-print s['temperatureMax'].mean()
-print s['temperatureMax'].median()
-
-print s['temperatureMin'].min()
-print s['temperatureMin'].max()
-print s['temperatureMin'].mean()
-print s['temperatureMin'].median()
+    'high_min': daily['temperatureMin'].max(),
+    'low_min': daily['temperatureMin'].min(),
+    'mean_min': daily['temperatureMin'].mean(),
+    'median_min': daily['temperatureMin'].median(),
+    'greatest_delta_min': daily['changeMin'].max(),
+    'smallest_delta_min': daily['changeMin'].min(),
+}
+print "BOSTON WEATHER\n"
+print "HIGHS\n" \
+      "Highest high: {high_max}\n" \
+      "Lowest high: {low_max}\n" \
+      "Median high: {median_max}\n" \
+      "Mean high: {mean_max}\n" \
+      "Greatest change: {greatest_delta_max}\n" \
+      "Smallest change: {smallest_delta_max}\n" \
+      "\n" \
+      "LOWS\n" \
+      "Highest high: {high_min}\n" \
+      "Lowest high: {low_min}\n" \
+      "Median high: {median_min}\n" \
+      "Mean high: {mean_min}\n" \
+      "Greatest change: {greatest_delta_min}\n" \
+      "Smallest change: {smallest_delta_min}\n".format(**values)
--- unit_3/weather_utils.py
+++ unit_3/weather_utils.py
@@ -1,123 +1,91 @@
-__author__ = 'me'
-
-import datetime
-import json
-import requests
-import sqlite3 as lite
 import pandas as pd
-import datetime
-import forecast_io as forecast
+import data_source as data_source
 
 
-CITIES = {"Atlanta": (33.762909,-84.422675),
-          "Austin": (30.303936,-97.754355),
-          "Boston": (42.331960,-71.020173),
-          "Chicago": (41.837551,-87.681844),
-          "Cleveland": (41.478462,-81.679435)
-          }
+def load_30_days():
+    """
+    Load data from the last 30 days from the data source
+    :return: json of the last 30 days
+    """
+    return data_source.load_30_days()
 
-DATA_DIR="./data/"
 
 # *============= DATA MUNGING =========* #
 
-## load the last 30 days of data.
-## See note in __main__ at bottom about how boston_last_30.json was assembled
-def load_30_days():
-    datafile = DATA_DIR + "/boston_last_30.json"
-    with open(datafile) as json_src:
-        content = json.load(json_src)
-        return content
-
-def read_daily_json(content):
-    for item in content:
-        yield item['daily']['data'][0]
-
-
 
 def extract_daily_from_json(content):
+    """
+    Given a Forecast.io json response, pull out all the Daily data
+    :param content: json response from Forecast.io
+    :return: a data frame containing the json from daily:{...}
+    """
     dailies = []
     for item in content:
-        i = item['daily']['data'][0]
-        #i['date']= datetime.datetime.fromtimestamp(
-        #        int(i['apparentTemperatureMinTime'])
-        #    ).strftime('%Y-%m-%d')
         dailies.extend(item['daily']['data'])
-    return pd.DataFrame(dailies)
+
+    df = pd.DataFrame(dailies)
+    df.sort(columns='apparentTemperatureMinTime', ascending=True, inplace=True)
+    add_temp_deltas(df)
+    return df
 
 
 def extract_hourly_from_json(content):
+    """
+    Given a Forecast.io json response, pull out all the Hourly data
+    :param content: json response from Forecast.io
+    :return: a data frame containing the json from hourly:[{...}]
+    """
     hourlies = []
     for item in content:
         hourlies.extend(item['hourly']['data'])
     hourly_df = pd.DataFrame(hourlies)
     return hourly_df
 
+def add_temp_deltas(data):
+    """
+    Calculate the change in max and min temps between days
 
-# *================ CACHED DATA ========* #
+    :param data: a data frame sorted in ascending date order
+    :return: data with changeMax and changeMin columns
+    """
+    last = None
+    deltaMax = []
+    deltaMin = []
 
-def cache_filepath(lat_lon, timestamp):
-    return DATA_DIR + "%f,%f,%d.json" % (lat_lon[0], lat_lon[1], timestamp)
+    for index, item in data.iterrows():
+        if last is None:
+            deltaMax.append(0)
+            deltaMin.append(0)
+        else:
+            deltaMax.append(abs(item['temperatureMax'] - last['temperatureMax']))
+            deltaMin.append(abs(item['temperatureMin'] - last['temperatureMin']))
 
+        last = item
 
-def get_cached_response(lat_lon, timestamp):
-    try:
-        with open(cache_filepath(lat_lon, timestamp), 'r') as fd:
-            return json.load(fd, encoding='utf-8')
-    except IOError:
-        print "No such file"
-        return None
-
-def write_to_cache(obj, lat_lon, timestamp):
-    try:
-        with open(cache_filepath(lat_lon, timestamp), 'w') as fd:
-            fd.write( json.dumps(obj, encoding='utf-8'))
-    except IOError:
-        print "Could not write file"
-        return None
-
-# *============== GENERAL =========* #
-
-def get_unix_timestamp(offset):
-    when = datetime.datetime.utcnow()
-    when = when - datetime.timedelta(days=offset,
-                                     hours=when.hour,
-                                     minutes=when.minute,
-                                     seconds=when.second,
-                                     microseconds=when.microsecond)
-    epoch_seconds = (when - datetime.datetime(1970,1,1)).total_seconds()
-    return int(epoch_seconds)
-
-
-def fetch_weather_data(lat_lon, offset):
-    timestamp = get_unix_timestamp(offset)
-    obj = get_cached_response(lat_lon, timestamp)
-    if not obj:
-        print "Fetching data the hard way"
-        obj = forecast.request_data(lat_lon, timestamp)
-        write_to_cache(obj, lat_lon, timestamp)
-    return obj
+    data['changeMax'] = [x for x in deltaMax]
+    data['changeMin'] = [y for y in deltaMin]
 
 
 
 def populate_30_days():
-    for days_past in range(0,30):
-        fetch_weather_data(CITIES['Boston'], days_past)
+    """
+    Request weather data for the last 30 days.
+    :return: None
+    """
+    CITIES = {"Atlanta": (33.762909, -84.422675),
+              "Austin": (30.303936, -97.754355),
+              "Boston": (42.331960, -71.020173),
+              "Chicago": (41.837551, -87.681844),
+              "Cleveland": (41.478462, -81.679435)
+              }
 
+    for days_past in range(0, 30):
+        data_source.fetch_weather_data(CITIES['Boston'], days_past)
 
-#* ============= MAIN ======================* #
+# * ============= MAIN ======================* #
 
 if __name__ == '__main__':
-
     # Run the file directly to pull down the data files
     # This happens once, so __main__ is fine.
     print('Populating data...')
     populate_30_days()
-
-    # The file boston_last_30.json was compiled manually from the daily json files
-    # From the command line,
-    #       cat 42*json >> boston_last_30.json
-    # followed by correcting the json in emacs, to wit:
-    #    * separating entries with commas
-    #    * enclosing the json with an opening [ and ending ]
-    # Possible in python, but much faster by hand
-
